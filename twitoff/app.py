0b839464f3af98a5456263bfe6eba214c6239125
Kelli Scheuble <kellischeuble@gmail.com>
2020-03-22 16:23:43 -0700

    adds about and home page

--- twitoff/app.py
+++ twitoff/app.py
@@ -8,15 +8,37 @@
 # FLASK_APP=twitoff:APP flask shell
 
 from flask import Flask
+from .models import DB
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     # make the app
     # __name__ returns the name of the module that we are running the code from
     app = Flask(__name__)
+    # three slashes make it a relatitive path
+    # set it up so that the app knows about the database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # now make it so that the database knows about the app
+    DB.init_app(app)
+
 
     # set route up and listen to the route
+    # here, we are writing a function that will return the information that
+    # will be displayed on the website.. the '/' is the homepage.
     @app.route('/')
-    def root():
+    @app.route('/home')
+    def home():
+        # this will be shown when we are on the home page
         return 'Welcome to TwitOff!'
+    @app.route('/about')
+    def about():
+       # this will be shown when we are on the about page
+       return 'About this app!'
+
+    # put debug mode on so that we don't have to restart our server each time
+    # we want to view a change
+    # THIS ISN'T WORKING
+    if __name__ == '__main__':
+        app.run(debug=True)
+
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,6 +1,6 @@
 """SQLAlchemy models for TwitOff."""
 
-from flask.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 # make database
 DB = SQLAlchemy()
@@ -9,7 +9,7 @@ DB = SQLAlchemy()
 # we are using sqlite for now
 # a model is a class
 # the model (here it is User, is a subclass of the Model parent class)
-class User(Db.Model):
+class User(DB.Model):
     """Twitter users that we pull and analyze Tweets for."""
     # define fields
     # use types.. we will want to use generic types 
@@ -20,9 +20,28 @@ class User(Db.Model):
     # nullable=False because it is required.
     handle = DB.Column(DB.String(15), nullable=False)
 
+    # define representation method to give us the string that we 
+    # will get when looking in the terminal
+    def __repr__(self):
+        return f'<User {self.handle}>'
+
+
 # make Tweet model
 class Tweet(DB.Model):
     """Tweets."""
     id = DB.Column(DB.Integer, primary_key=True)
     # max length for a tweet is 280
-    test = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(280))
+    # going to add a reference from tweet to user ID so we
+    # can make a relational database with a one to many relationship
+    # (one person, many tweets)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    # make a user field. this won't change the database, but it will allow us to return
+    # user object when using the command line
+    # allows us to get from tweets to user, and from user to tweets... generated at execution
+    # time (not in the database because that would be inneficient)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    # define representation
+    def __repr__(self): 
+       return f'<Tweet {self.text}' 
